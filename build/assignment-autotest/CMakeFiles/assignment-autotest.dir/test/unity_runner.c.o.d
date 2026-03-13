ba20e8779d9276c0ed542d13f80e6715eef817b0
Mohamed  Fouad <mohfou9z@orw-mfouad-01.wv.mentorg.com>
2026-03-13 15:26:15 +0000

    assignemet1

---
+++ build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
@@ -0,0 +1,3 @@
+assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
+ /wv/mohfou9z/Embedded_Linux/assignment-1-mafouadm/assignment-autotest/test/unity_runner.c \
+ /usr/include/stdc-predef.h
